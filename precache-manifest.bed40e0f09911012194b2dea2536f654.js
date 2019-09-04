1372b64d2c7ab5177117ce956e3b65ba75e3e016
allytang <allytang2015@gmail.com>
2019-09-04 14:14:57 +1000

    Updates

--- precache-manifest.bed40e0f09911012194b2dea2536f654.js
+++ precache-manifest.bed40e0f09911012194b2dea2536f654.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aa079badae7d9eebe3bdd89c8b186e3f",
+    "revision": "a637b9a097f77dea88cf85fb7bb9bd22",
     "url": "/react-recipe-app/index.html"
   },
   {
-    "revision": "b7b041a415b6dc383210",
+    "revision": "a6b0b1904803c623ea3b",
     "url": "/react-recipe-app/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "c969e8785f2d382041b5",
+    "revision": "d62525ac4d41d79e3b55",
     "url": "/react-recipe-app/static/css/main.c8d7f973.chunk.css"
   },
   {
-    "revision": "b7b041a415b6dc383210",
-    "url": "/react-recipe-app/static/js/2.8fd6a92f.chunk.js"
+    "revision": "a6b0b1904803c623ea3b",
+    "url": "/react-recipe-app/static/js/2.4b03f742.chunk.js"
   },
   {
-    "revision": "c969e8785f2d382041b5",
-    "url": "/react-recipe-app/static/js/main.91cc3e1a.chunk.js"
+    "revision": "d62525ac4d41d79e3b55",
+    "url": "/react-recipe-app/static/js/main.7e154058.chunk.js"
   },
   {
     "revision": "a541b5dc08a38dc79315",
